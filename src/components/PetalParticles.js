b6edc875f352e1fc0c56b45d8c70ca5c32b23600
quadiez <dduidokdoi@gmail.com>
2022-03-05 01:15:05 +0700

    feat: home

---
+++ src/components/PetalParticles.js
@@ -0,0 +1,115 @@
+import Particles from "react-tsparticles"
+
+const particleConfig = {
+    "fullScreen": {
+        "zIndex": 5
+    },
+    "particles": {
+        "move": {
+            "direction": "bottom-right",
+            "attract": {
+                "rotate": {
+                    "x": 600,
+                    "y": 1200
+                }
+            },
+            "enable": true,
+            "outModes": {
+                "bottom": "out",
+                "left": "out",
+                "right": "out",
+                "top": "out"
+            }
+        },
+        "number": {
+            "density": {
+                "enable": true
+            },
+            "value": 30
+        },
+        "opacity": {
+            "random": {
+                "enable": true
+            },
+            "value": {
+                "min": 0.1,
+                "max": 1
+            },
+            "animation": {
+                "enable": true,
+                "speed": 0.5,
+                "minimumValue": 0.2
+            }
+        },
+        "rotate": {
+            "random": {
+                "enable": true
+            },
+            "animation": {
+                "enable": true,
+                "speed": 3
+            },
+            "direction": "random"
+        },
+        "shape": {
+            "options": {
+                "image": [
+                    {
+                        "src": "/petals/petal1.png",
+                        "width": 32,
+                        "height": 32
+                    },
+                    {
+                        "src": "/petals/petal2.png",
+                        "width": 32,
+                        "height": 32
+                    },
+                    {
+                        "src": "/petals/petal2.png",
+                        "width": 32,
+                        "height": 32
+                    },
+                    {
+                        "src": "/petals/petal2.png",
+                        "width": 32,
+                        "height": 32
+                    },
+                    {
+                        "src": "/petals/petal2.png",
+                        "width": 32,
+                        "height": 32
+                    },
+                ]
+            },
+            "type": "image"
+        },
+        "size": {
+            "value": 25,
+            "animation": {
+                "speed": 40,
+                "minimumValue": 0.1
+            }
+        },
+    }
+}
+
+export const PetalParticles = () => {
+    const particlesInit = (main) => {
+        console.log(main);
+
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    };
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={particleConfig}
+        />
+    )
+}
---
+++ src/sections/Home.js
@@ -0,0 +1,20 @@
+import { Image } from "@chakra-ui/image"
+import { Box, Flex, Heading } from "@chakra-ui/layout"
+import { Text } from "@chakra-ui/react"
+import { PetalParticles } from "../components/PetalParticles"
+
+export const Home = () => {
+    return (
+        <Flex w="100vw" h="100vh" direction={["row", "column"]} justify="center" align={["flex-start", "flex-end"]}>
+            <Flex position="absolute" zIndex="10" w="100%" top="0" justify={["center", "flex-start  "]} p={5} pt={['6vh', 5]}>
+                <Image src="icon.svg" boxSize="6vh" />
+            </Flex>
+            <Flex direction="column" justify="center" align={["center", "flex-end"]} color={["#9F8280", "white"]} pr={[0, 16]} pb={[0, 6]} pt={["15vh", 0]}>
+                <Heading fontSize={["6vh", "7vw"]} fontWeight="light">Congratulations</Heading>
+                <Text fontSize={["2vh", "2vw"]} mt={["0.8vh", "-2vh"]} >11.03.2565 | From 6/1 KMS 88 :)</Text>
+            </Flex>
+            <PetalParticles />
+            <Box w="100vw" h="100vh" bgImage={["url('/phone_bg.png')", "url('/pc_bg.png')"]} bgSize="cover" zIndex="-200" position="absolute" />
+        </Flex >
+    )
+}
